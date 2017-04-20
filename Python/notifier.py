970d498f3c1163344fd30368e0a5ff6b240bf3c0
Luiz Henrique Zambom Santana <lhzsantana@gmail.com>
2017-04-20 12:38:45 -0300

    Smaill fixes

--- Python/notifier.py
+++ Python/notifier.py
@@ -1,22 +1,53 @@
 import redis
 import json
+import time
 import smtplib
+import threading
 
 r_server = redis.Redis('192.168.99.100')
 
-for key in r_server.scan_iter(match='NOTIF*'):
-  obj = json.loads(r_server.get(key))
+def save_to_redis(key, obj):
+   r_server.lpush(key["email"],obj["message"])
+   r_server.incr("counter_ps")
 
-  if obj["status"]=="ADDED":
+def send_email(key, obj):
+   r_server.incr("counter_email")
 
-      obj["status"]="PROCESSING"
-      r_server.set("NOTIFICATION:"+obj["uuid"],json.dumps(obj));
+def notify_ps(obj):
+    for key in obj["subscribers"]:
+        t = threading.Thread(target=save_to_redis, args = (key, obj,))
+        t.daemon = True
+        t.start()
 
-      if "PUSH_SUBSCRIBE" in obj["channels"]:
-         for key in obj["subscribers"]:
-            r_server.lpush(key["email"],obj["message"])
-      if "EMAIL" in obj["channels"]:
-         for key in obj["subscribers"]:
-            print('a')
-      obj["status"]="COMPLETED"
-      r_server.set("NOTIFICATION:"+obj["uuid"],json.dumps(obj));
+    obj["status"]="COMPLETED"
+    r_server.set("NOTIFICATION:"+obj["uuid"],json.dumps(obj));
+
+def notify_email(obj):
+    for key in obj["subscribers"]:
+       t = threading.Thread(target=send_email, args = (key, obj,))
+       t.daemon = True
+       t.start()
+
+    obj["status"]="COMPLETED"
+    r_server.set("NOTIFICATION:"+obj["uuid"],json.dumps(obj));
+
+while True:
+    for key in r_server.scan_iter(match='NOTIF*'):
+
+        obj = json.loads(r_server.get(key))
+
+        if obj["status"]=="ADDED":
+            obj["status"]="PROCESSING"
+            r_server.set("NOTIFICATION:"+obj["uuid"],json.dumps(obj));
+
+            if "PUSH_SUBSCRIBE" in obj["channels"]:
+                t = threading.Thread(target=notify_ps, args = (obj,))
+                t.daemon = True
+                t.start()
+
+            if "EMAIL" in obj["channels"]:
+                t = threading.Thread(target=notify_email, args = (obj,))
+                t.daemon = True
+                t.start()
+    time.sleep(2)
+
